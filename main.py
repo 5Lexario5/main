533a153af0f41fbd137bf80b021bb12fd016d50b
Lexario <LEXARIO2204@yandex.ru>
2022-10-10 10:17:53 +0300

    Find max square of triangles

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-print("Введите коэффициенты уравнения")
-print("ax^2 + bx + c = 0:")
-a = float(input("a = "))
-b = float(input("b = "))
-c = float(input("c = "))
-
-dis = b ** 2 - 4 * a * c
-print("Дискриминант D =", dis)
-
-if dis > 0:
-    x1 = (-b + dis ** 0.5) / (2 * a)
-    x2 = (-b - dis ** 0.5) / (2 * a)
-    print("x1 = ", x1 )
-    print("x2 = ", x2 )
-elif dis == 0:
-    x = -b / (2 * a)
-    print("x = ",x)
-else:
-    print("Корней нет")
+sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
+sides = sides = sorted(sides, reverse=True)
+smax = 0
+for i in range(len(sides)):
+    for j in range(i + 1, len(sides)):
+        for k in range(j + 1, len(sides)):
+            a = sides[i]
+            b = sides[j]
+            c = sides[k]
+            if a + b > c and a + c > b and b + c > a:
+                p = (a + b + c) / 2
+                s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
+                if s > smax:
+                    smax = s
+print("Максимальная площадь треугольника", smax)
